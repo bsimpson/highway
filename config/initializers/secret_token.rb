8ec46f64dcfcddea4ed47383b16c0647a8897075
Ben Simpson <thehoagie@gmail.com>
2013-05-21 22:25:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Highway::Application.config.secret_token = '1ff3a4232a2474eca1f8976dac129b084bc9e08fb24c76ce7ee64b850c82c4d2a7dfa212093017fb145c8bca9c25912e8d3768fd7b3984c9ded346c4163a38e7'
